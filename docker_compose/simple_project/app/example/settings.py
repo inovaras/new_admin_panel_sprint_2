667c7a9168bc75dfceca155b5004cad8f1b370d3
Albina Gilmutdinova <inovaaras@yandex.ru>
2024-07-30 00:26:03 +0500

    собран и запущен docker-compose

--- docker_compose/simple_project/app/example/settings.py
+++ docker_compose/simple_project/app/example/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'example.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'NAME': 'movies_database',
+        'USER': 'admin',
+        'PASSWORD': '123123',
         'HOST': os.getenv('SQL_HOST', '127.0.0.1'),
         'PORT': os.getenv('SQL_PORT', 5432),
         'OPTIONS': {
--- lesson_example_5(web-server)/my_server.py
+++
@@ -1,3 +0,0 @@
-def hello(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain, charset=utf-8')])
-    yield 'Hello PRACTIKUM!\n'.encode('utf-8')
