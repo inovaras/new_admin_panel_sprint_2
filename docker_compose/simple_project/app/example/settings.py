194ca259e966533dd38401cd53e915b9e2a4714e
Albina Gilmutdinova <inovaaras@yandex.ru>
2024-07-31 17:55:12 +0500

    сделана вторая часть

--- docker_compose/simple_project/app/example/settings.py
+++ docker_compose/simple_project/app/example/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movies',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'example.urls'
@@ -134,3 +137,4 @@ MEDIA_ROOT = '/var/www/media/'  # или '/opt/app/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8080",]
--- docker_compose/simple_project/app/movies/api/v1/views.py
+++ docker_compose/simple_project/app/movies/api/v1/views.py
@@ -1,5 +1,71 @@
-from django.http import HttpResponse
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models import Q
+from django.http import JsonResponse
+from django.views import View
+from django.views.generic.list import BaseListView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from ...models import FilmWork, Genre, Person
 
 
-def api(request):
-    return HttpResponse("My best API")
+class MoviesApiMixin:
+    model = FilmWork
+    http_method_names = ['get']
+
+    def get_queryset(self):
+        return FilmWork.objects.prefetch_related('genres', 'persons').values(
+            'id', 'title', 'description', 'creation_date', 'rating', 'type'
+        ).annotate(
+            genres=ArrayAgg('genres__name', distinct=True),
+            actors=ArrayAgg('persons__full_name', filter=Q(personfilmwork__role='actor'), distinct=True),
+            directors=ArrayAgg('persons__full_name', filter=Q(personfilmwork__role='director'), distinct=True),
+            writers=ArrayAgg('persons__full_name', filter=Q(personfilmwork__role='writer'), distinct=True),
+        )
+
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(context, **response_kwargs)
+
+
+class BaseDetailView(View):
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
+
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(context)
+
+
+class MoviesListApi(MoviesApiMixin, BaseListView):
+    paginate_by = 10  # Количество объектов на странице
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        return self.render_to_response(context)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        queryset = self.get_queryset()
+        paginator = Paginator(queryset, self.paginate_by)
+
+        page = self.request.GET.get('page')
+        try:
+            movies = paginator.page(page)
+        except PageNotAnInteger:
+            movies = paginator.page(1)
+        except EmptyPage:
+            movies = paginator.page(paginator.num_pages)
+
+        context = {
+            'count': paginator.count,
+            'total_pages': paginator.num_pages,
+            'prev': movies.previous_page_number() if movies.has_previous() else None,
+            'next': movies.next_page_number() if movies.has_next() else None,
+            'results': list(movies),
+        }
+        return context
+
+
+class MoviesDetailApi(MoviesApiMixin, BaseDetailView):
+    def get_context_data(self, **kwargs):
+        movie_id = kwargs.get('pk')
+        movie = self.get_queryset().filter(id=movie_id).first()
+        return movie if movie else {}
